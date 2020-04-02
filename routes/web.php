7df1553e65afb8ebd304faed4fa82d7cd167d13b
AnjiSugidjaja <anjisugidjaja@gmail.com>
2020-04-03 01:30:06 +0700

    Menambahkan input untuk tambah file

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::post('suratmasuk', 'SuratMasukControllerk@store');
 
 Route::get('suratkeluar', 'SuratKeluarController@index');
 Route::get('suratkeluar/tambah', 'SuratKeluarController@tambah');
-Route::post('suratkeluar/store', 'SuratKeluarController@store');
+Route::post('/suratkeluar/store', 'SuratKeluarController@store');
 Route::get('suratkeluar/edit/{id_suratkeluar}', 'SuratKeluarController@edit');
 Route::post('suratkeluar/update/{id_suratkeluar}', 'SuratKeluarController@update');
 Route::get('suratkeluar/hapus/{id_suratkeluar}', 'SuratKeluarController@delete');
