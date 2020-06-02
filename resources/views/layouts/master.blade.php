16fbdd8d38c4daa70515588e7cabf63bbd48c421
gugi97 <gugigerar97@gmail.com>
2020-06-02 12:44:36 +0700

    delete upload image & modified master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -136,14 +136,14 @@
             <!-- Sidebar -->
             <div class="sidebar">
                 <!-- Sidebar user (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
                         <img src="/adminlte/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">{{ Auth::user()->name }}</a>
                     </div>
-                </div>
+                </div> -->
 
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
