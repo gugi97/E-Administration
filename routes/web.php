5bfe3726e01a24d954982d56494ff485382d9298
AnjiSugidjaja <anjisugidjaja@gmail.com>
2020-06-28 23:16:59 +0700

    Memberi Hak Akses setiap Status

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,10 @@ Route::group(['middleware' => ['auth','checkStatus:Admin']],function(){
     Route::get('user/edit/{id}', 'UserController@edit');
     Route::put('user/update/{id}', 'UserController@update');
     Route::get('user/hapus/{id}', 'UserController@delete');
-    Route::resource('kaprodi', 'KaprodiController');
 });
 
 Route::group(['middleware' => ['auth','checkStatus:Admin,Karyawan,Dekan,Ketua Program Studi']],function(){
     Route::get('/', 'HomeController@index')->name('home');
-
-    Route::get('suratkeluar', 'SuratKeluarController@index')->name('suratkeluar');
-    Route::get('arsipsuratkeluar', 'ArsipSuratKeluarController@index')->name('arsipsuratkeluar');
-    Route::get('arsipfilesuratkeluar', 'ArsipSuratKeluarController@indexfile')->name('arsipfilesuratkeluar');
-    Route::get('suratkeluar/tambah', 'SuratKeluarController@tambah');
-    Route::post('suratkeluar/store', 'SuratKeluarController@store');
-    Route::get('suratkeluar/edit/{id_suratkeluar}', 'SuratKeluarController@edit');
-    Route::put('suratkeluar/update/{id_suratkeluar}', 'SuratKeluarController@update');
-    Route::get('suratkeluar/hapus/{id_suratkeluar}', 'SuratKeluarController@delete');
 
     Route::get('suratmasuk', 'SuratMasukController@index')->name('suratmasuk');
     Route::get('arsipsuratmasuk', 'ArsipSuratMasukController@index')->name('arsipsuratmasuk');
@@ -41,5 +31,19 @@ Route::group(['middleware' => ['auth','checkStatus:Admin,Karyawan,Dekan,Ketua Pr
     Route::get('suratmasuk/edit/{id_suratmasuk}', 'SuratMasukController@edit');
     Route::put('suratmasuk/update/{id_suratmasuk}', 'SuratMasukController@update');
     Route::get('suratmasuk/hapus/{id_suratmasuk}', 'SuratMasukController@delete');
+});
 
+Route::group(['middleware' => ['auth','checkStatus:Admin,Dekan,Ketua Program Studi']],function(){
+    Route::get('suratkeluar', 'SuratKeluarController@index')->name('suratkeluar');
+    Route::get('arsipsuratkeluar', 'ArsipSuratKeluarController@index')->name('arsipsuratkeluar');
+    Route::get('arsipfilesuratkeluar', 'ArsipSuratKeluarController@indexfile')->name('arsipfilesuratkeluar');
+    Route::get('suratkeluar/tambah', 'SuratKeluarController@tambah');
+    Route::post('suratkeluar/store', 'SuratKeluarController@store');
+    Route::get('suratkeluar/edit/{id_suratkeluar}', 'SuratKeluarController@edit');
+    Route::put('suratkeluar/update/{id_suratkeluar}', 'SuratKeluarController@update');
+    Route::get('suratkeluar/hapus/{id_suratkeluar}', 'SuratKeluarController@delete');
+});
+
+Route::group(['middleware' => ['auth','checkStatus:Admin,Ketua Program Studi']],function(){
+    Route::resource('kaprodi', 'KaprodiController');
 });
