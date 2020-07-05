3e4547a75f4b32ce3643af7f009e820a17b3d52e
AnjiSugidjaja <anjisugidjaja@gmail.com>
2020-07-06 00:15:20 +0700

    Penambahan Agenda Surat Masuk dan Surat Keluar serta Cetak PDF

---
+++ app/Http/Controllers/AgendaSuratMasukController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SuratMasuk;
+use App\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use PDF;
+
+class AgendaSuratMasukController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        // mengambil data dari table suratmasuk
+        $agendasuratmasuk = SuratMasuk::getallsuratmasuk();
+        $orders = null;
+
+    	// mengirim data suratmasuk ke view index
+		return view('agendasuratmasuk',['agendasuratmasuk' => $agendasuratmasuk, 'orders' => $orders]);
+    }
+
+    public function cari(Request $request)
+    {
+        $dari_tgl = $request->input('dari_tgl');
+        $sampai_tgl = $request->input('sampai_tgl');
+
+        $orders = SuratMasuk::whereBetween('tgl_surat', [$dari_tgl, $sampai_tgl])->get();
+
+        return view('agendasuratmasuk', compact('orders'), ['dari_tgl'=>$dari_tgl, 'sampai_tgl'=>$sampai_tgl]);
+    }
+
+    public function cetak_pdf(Request $request)
+    {
+        $dari_tgl2 = $request->input('dari_tgl2');
+        $sampai_tgl2 = $request->input('sampai_tgl2');
+
+    	$data = SuratMasuk::whereBetween('tgl_surat', [$dari_tgl2, $sampai_tgl2])->get();
+ 
+    	$pdf = PDF::loadview('agendasuratmasuk_pdf',['data'=>$data, 'dari_tgl2'=>$dari_tgl2, 'sampai_tgl2'=>$sampai_tgl2]);
+    	return $pdf->stream();
+    }
+}
